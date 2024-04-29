afb1bc8c65f4e437cd57d3c9919ba5e237c93687
AnnaKop1705 <kopeikinaann@gmail.com>
2024-04-29 16:44:43 +0300

    add score

--- main.py
+++ main.py
@@ -18,15 +18,24 @@ target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+font = pygame.font.Font(None, 36)
+
+def show_score(score):
+    score_text = font.render('Очки: ' + str(score), True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
+
+score = 0
 running = True
 while running:
     screen.blit(background_img, (0, 0))
+    show_score(score)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                score += 1
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
